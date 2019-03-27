e1c8519edb727f884e657db9c7164bcc53d30df7
Matt Bailey <matt.bailey@bethesda.net>
2019-03-27 08:21:53 -0700

    Add Bethesdanet

---
+++ stts/Services/StatusPage/Bethesdanet.swift
@@ -0,0 +1,11 @@
+//
+//  Bethesdanet.swift
+//  stts
+//
+
+import Foundation
+
+class Bethesdanet: StatusPageService {
+    let url = URL(string: "https://bethesdapublic.statuspage.io")!
+    let statusPageID = "p6r5nl77dwjf"
+}
